b8dad2b6ee8e5b704c4353aab30d0f036f393b14
DavidFernandezUD <david.f@opendeusto.es>
2024-03-29 21:36:47 +0100

    Precalculate magic numbers

--- src/main.c
+++ src/main.c
@@ -8,26 +8,16 @@
 
 int main() {
 
-    // srand(time(NULL));
-    init_magic_numbers();
-
-    init_bishop_attacks();
-    init_rook_attacks();
+    init_piece_attacks();
 
     uint64_t occupancy = 0ULL;
 
-    SET_BIT(occupancy, f5);
+    SET_BIT(occupancy, f7);
     SET_BIT(occupancy, b5);
     SET_BIT(occupancy, e8);
     SET_BIT(occupancy, e4);
 
     print_bitboard(get_bishop_attacks(d5, occupancy));
-
-    // print_bitboard(occupancy);
-
-    // print_bitboard(get_bishop_attacks(d5, occupancy));
-
-    // NOTES: mn = 64
 
     return 0;
 }
